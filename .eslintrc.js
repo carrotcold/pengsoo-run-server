a7709dc0493680d069349ad0017131a3cd4b27b3
Soin Na <carrotcold@gmail.com>
2020-12-11 03:19:23 +0900

    :arrow_down: fix redis version for compatibility

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -18,7 +17,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
 };
