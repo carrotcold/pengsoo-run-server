c223b78f059aaa4a0193c977593a487181afe211
Soin Na <carrotcold@gmail.com>
2020-12-16 04:36:48 +0900

    🔧 set openSSL for https server

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,20 @@ import * as helmet from 'helmet';
 
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './adapters/redis.adapter';
+import { develop, prod } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: {
-      origin: '*',
-      optionsSuccessStatus: 200,
-    },
-  });
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    process.env.NODE_ENV === 'development' ? develop : prod,
+  );
 
+  console.log('✅process.env.NODE_ENV', process.env.NODE_ENV);
   app.use(helmet());
   app.useWebSocketAdapter(new RedisIoAdapter(app));
 
   const port = process.env.SERVER_PORT || 8080;
+
   await app.listen(port, () => console.log(`listening on ${port}`));
 }
 
