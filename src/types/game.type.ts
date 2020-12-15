d0f7fc4a9498016725b138752259d6ca7093df59
Soin Na <carrotcold@gmail.com>
2020-12-15 23:38:41 +0900

    🏷️ define game types

---
+++ src/types/game.type.ts
@@ -0,0 +1,25 @@
+export const enum GameMode {
+  P1 = '1 Player',
+  P2 = '2 Player',
+  P3 = '3 Player',
+}
+
+export const enum GameRole {
+  ALL = 'all',
+  LR = 'left-right',
+  J = 'jump',
+  L = 'left',
+  R = 'right',
+}
+
+export interface Player {
+  id: string;
+  role: GameRole;
+}
+
+export interface Game {
+  id: string | null;
+  mode: GameMode | null;
+  playerList: Player[];
+  remainingRole: GameRole[];
+}
