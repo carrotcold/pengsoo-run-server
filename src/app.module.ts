166f4090a1e941a6db4e014aa91ba0eeabd6aa1f
Soin Na <carrotcold@gmail.com>
2020-12-22 19:56:36 +0900

    ✨ add home router for server health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { RedisCacheModule } from './redisCache/redisCache.module';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
+import { RedisCacheModule } from './redisCache/redisCache.module';
 
 @Module({
   imports: [
